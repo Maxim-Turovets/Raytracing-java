6bb288dbb1c2b06b9eaa1a434ddf31ff356dc4c8
Maxim-Turovets <mturovec71@gmail.com>
2021-01-17 17:06:35 +0200

    Init project structure

---
+++ src/main/java/com/project/raytracing/RaytracingApplication.java
@@ -0,0 +1,13 @@
+package com.project.raytracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaytracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaytracingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/raytracing/RaytracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.raytracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaytracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
